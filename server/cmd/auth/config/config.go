729f10e2297e6f6b01e18d326d2e746ab9ec4538
jjeejj <jwj@wenjunjiang.com>
2023-02-22 13:32:57 +0800

    chore: 移除无用的代码

--- server/cmd/auth/config/config.go
+++ server/cmd/auth/config/config.go
@@ -24,14 +24,9 @@ type WXConfig struct {
 }
 
 type ServerConfig struct {
-	Name        string        `mapstructure:"name" json:"name"`
-	Host        string        `mapstructure:"host" json:"host"`
-	MysqlInfo   mysql.Config  `mapstructure:"mysql" json:"mysql"`
-	OtelInfo    OtelConfig    `mapstructure:"otel" json:"otel"`
-	WXInfo      WXConfig      `mapstructure:"wx_config" json:"wx_config"`
-	BlobSrvInfo BlobSrvConfig `mapstructure:"blob_srv" json:"blob_srv"`
-}
-
-type BlobSrvConfig struct {
-	Name string `mapstructure:"name" json:"name"`
+	Name      string       `mapstructure:"name" json:"name"`
+	Host      string       `mapstructure:"host" json:"host"`
+	MysqlInfo mysql.Config `mapstructure:"mysql" json:"mysql"`
+	OtelInfo  OtelConfig   `mapstructure:"otel" json:"otel"`
+	WXInfo    WXConfig     `mapstructure:"wx_config" json:"wx_config"`
 }
